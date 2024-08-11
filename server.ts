1146ccca6a403315684f21605c1a4daa96182071
Karandeep Singh <karandeep.singh@sindia.co.in>
2024-08-11 14:09:15 +0530

    Add database setup

--- server.ts
+++ server.ts
@@ -1,7 +1,10 @@
+// import { connect } from "http2";
 import app from "./src/app";
 import { config } from "./src/config/config";
+import connectDB from "./src/config/db";
+const startServer = async () => {
+  await connectDB();
 
-const startServer = () => {
   const port = config.port || 3000;
   app.listen(port, () => {
     console.log(`Listening on port : ${port}`);
